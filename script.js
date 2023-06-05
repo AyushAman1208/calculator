b14b5af59100a648410722a329a07a1fd8864e96
Ayush Aman <ayushaman1208@gmail.com>
2023-06-05 11:29:31 +0530

    Improvements in logic

--- script.js
+++ script.js
@@ -1,70 +1,51 @@
-console.log('Hey there')
+console.log("Hey there");
 
-let result=0
-let currNum=''
+let result = 0;
+let currNum = "0";
 
-let currChar=''
+let lastOp = "+";
 
-function calculations(){
+let currChar = "";
+operations = ["+", "-", "/", "x", "x>", "="];
 
-    
-    
+function calculations() {
+  if (operations.includes(currChar)) {
+    if (lastOp == "+") result += parseFloat(currNum);
+    else if (lastOp == "-") result -= parseFloat(currNum);
+    else if (lastOp == "x") result *= parseFloat(currNum);
+    else if (lastOp == "/") result /= parseFloat(currNum);
+    else if (currChar == "x>") {
+      currNum = currNum.substring(0, currNum.length - 1);
+    } 
+    else if (currChar == "=") {
+      console.log(result);
+      if (lastOp == "+") result += parseFloat(currNum);
+      else if (lastOp == "-") result -= parseFloat(currNum);
+      else if (lastOp == "x") result *= parseFloat(currNum);
+      else if (lastOp == "/") result /= parseFloat(currNum);
 
-    if(currChar=='+'){
-        result= result + parseInt(currNum)
-        currNum=''
+      let x = document.getElementsByClassName("results");
+      x[0].innerHTML = result;
+      result = 0;
+      currNum = "";
     }
 
-    else if(currChar=='x'){
-        result*=parseInt(currNum)
-        currNum=''
-    }
-
-    else if(currChar=='/'){
-        result/=parseInt(currNum)
-        currNum=''
-    }
-
-    else if(currChar=='-'){
-        result-=parseInt(currNum)
-        currNum=''
-    }
-
-    else if(currChar=='x>'){
-        currNum=currNum.substring(0,currNum.length-1)
-        
-    }
-
-    else if(currChar=='='){
-        console.log(result)
-        
-
-        
-        let x = document.getElementsByClassName('results')
-        x[0].innerHTML=result
-        result=0
-        currNum=''
-        
-    }
-
-    else{
-        currNum+=currChar
-        console.log(currChar,currNum)
-    }
-    document.getElementById('input').innerHTML=currNum
-    if(currChar=='=')document.getElementById('input').innerHTML=0
-
+    lastOp = currChar;
+    currNum = "";
+  } else {
+    currNum += currChar;
+    console.log(currChar, currNum);
+  }
+  document.getElementById("input").innerHTML = currNum;
+  if (currChar == "=" || currNum==0) document.getElementById("input").innerHTML = 0;
 }
 
-Array.from(document.getElementsByTagName('button')).forEach((element)=>{
-    
-    element.addEventListener('click',(e)=>{
-        
-        let val=e.target.id
-        console.log(val)
-        console.log('reached here')
-        currChar=val
-        calculations()
-    })
-})
-
+Array.from(document.getElementsByTagName("button")).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let val = e.target.id;
+    // console.log(val);
+    // console.log("reached here");
+    currChar = val;
+    calculations();
+  });
+});
